ed773bdbbd5a1de70be2b4e6dfb5e2c34da597a7
marco paternosto frosi <marcopaternostofrosi@marcos-MacBook-Air.local>
2024-03-11 22:07:42 +0100

    lot of changes

--- src/keycloak-theme/account/i18n.ts
+++ src/keycloak-theme/account/i18n.ts
@@ -1,6 +1,11 @@
 import { createUseI18n } from "keycloakify/account";
 
 //NOTE: See src/login/i18n.ts for instructions on customization of i18n messages.
-export const { useI18n } = createUseI18n({});
+export const { useI18n } = createUseI18n({
+    en:{
+        tagLineIstituzione: "Tag line dell'Istituzione",
+        enteDiAppartenenza: "Ente di Appartenenza"
+    }
+});
 
 export type I18n = NonNullable<ReturnType<typeof useI18n>>;
