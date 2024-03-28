2838475f826790cc022bf3eb0a4b4d9b7ce0d4e7
marco <marco.paternosto@outlook.it>
2024-03-28 11:37:58 +0100

    removed login theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode, lazy, Suspense } from "react";
-import { kcContext as kcLoginThemeContext } from "./keycloak-theme/login/kcContext";
 import { kcContext as kcAccountThemeContext } from "./keycloak-theme/account/kcContext";
 
-const KcLoginThemeApp = lazy(() => import("./keycloak-theme/login/KcApp"));
 const KcAccountThemeApp = lazy(() => import("./keycloak-theme/account/KcApp"));
 
 // Important note:
@@ -18,11 +16,6 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Suspense>
             {(()=>{
-
-                if( kcLoginThemeContext !== undefined ){
-                    return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
-                }
-
                 if( kcAccountThemeContext !== undefined ){
                     return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
                 }
